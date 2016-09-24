cc27120e02287e3887653f9f9c90ed5b1d72e485
Michael Wojciechowski <wojciii@gmail.com>
2016-09-24 11:16:22 +0200

    Updated script name.

--- script.on.file/addon.py
+++ script.on.file/addon.py
@@ -5,17 +5,11 @@ import os
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-#line1 = "Hello World!"
-#line2 = "We can write anything we want here"
-#line3 = "Using Python"
- 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 
-# xbmcgui.Dialog().ok(addonname, line1, line2, line3)
-
 script_dir = os.path.expanduser('~/kodi')
-script = script_dir + "/" + "ignorefileOrDir"
+script = script_dir + "/" + "onkodiselection.sh"
 
 filename = sys.argv[1]
 directory = sys.argv[2]
