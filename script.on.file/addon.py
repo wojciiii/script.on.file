f1164f682caa0939f147ff69cf7efbbd7138fd8d
Michael Wojciechowski <wojciii@gmail.com>
2017-08-08 23:00:42 +0200

    Added settings.xml and using the configuration from python.

--- script.on.file/addon.py
+++ script.on.file/addon.py
@@ -9,7 +9,11 @@ addonname   = addon.getAddonInfo('name')
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 
-url="http://192.168.2.37:4444/post"
+# url="http://192.168.2.37:4444/post"
+url = addon.getSetting('URL')
+
+print 'Using URL: %s' % url
+
 payload = {'file': sys.argv[1],
            'directory': sys.argv[2]
            }
